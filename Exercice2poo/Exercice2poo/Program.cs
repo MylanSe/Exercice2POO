58884e71a548090c7dedd5eeeb9ec59a95d4164e
Mylan SEUX <mylan.seux@gmail.com>
2022-02-09 08:44:17 +0100

    Mon premier commit

---
+++ Exercice2poo/Exercice2poo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice2poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
